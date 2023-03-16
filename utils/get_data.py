432ab4cfa3fd1a69268c13a288d64c2b8b56d095
M. McConnaughey <mcmxl@reagan.com>
2023-03-15 20:23:13 -0600

    Update get_data.py

--- utils/get_data.py
+++ utils/get_data.py
@@ -1,13 +1,11 @@
 import json
 
-
 def get_data() -> dict:
     """Get Json data from file."""
     with open("inventory.json", "r+") as f:
         try:
             existing_items = json.load(f)
-
-        except ValueError:
+        except json.decoder.JSONDecodeError:
             existing_items = {}
             json.dump(existing_items, f, indent=4)
 
