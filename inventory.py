266a3a89a961a147ddcf4e82cb10bf728ed3725c
M. McConnaughey <mcmxl@reagan.com>
2023-03-15 19:10:47 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,80 +1,95 @@
 import json
-from utils.clear_screen import Clear_Screen
+from utils.clear_screen import clear_screen
 from utils.get_data import get_data
-
 
 class Inventory:
     """Add, remove and view inventory."""
 
-    def write_to_file(inventory_item) -> None:
+    def write_to_file(inventory_item):
         """Write to file."""
         with open("inventory.json", "w") as file:
             json.dump(inventory_item, file, indent=4)
 
+    def check_available_inventory():
+        inventory_item = get_data()
+        if not inventory_item:
+            print("No inventory available!\n")
+
     def add_inventory() -> dict:
         """Add items to inventory."""
+        clear_screen()
         inventory_item = get_data()
+        add_item = input("Enter item: ").strip()
         try:
-            add_item = input("Enter item: ").strip()
-            add_quantity = int(input("Enter quantity: "))
-            if add_item in inventory_item:
-                inventory_item[add_item] += add_quantity
-                print(f"{add_item} added to inventory.\n")
-            else:
-                inventory_item[add_item] = add_quantity
+            quantity = int(input("Enter quantity: "))
         except ValueError:
             print("Invalid Entry!")
-        Clear_Screen()
+            return inventory_item
+
+        if add_item in inventory_item:
+            inventory_item[add_item] += quantity
+        else:
+            inventory_item[add_item] = quantity
+
+        #clear_screen()
         print(f"{add_item} added to inventory.\n")
         Inventory.write_to_file(inventory_item)
+        return inventory_item
 
     def delete_item() -> dict:
         """Delete items from inventory."""
+        clear_screen()
         inventory_item = get_data()
-        try:
-            if inventory_item == {}:
-                print("Nothing to delete.\n")
-            else:
-                delete = input("Enter item to delete: ").strip()
-                inventory_item.pop(delete)
-                print(f"{delete} deleted!\n")
-                Inventory.write_to_file(inventory_item)
-        except KeyError as error:
-            print(error)
+        if not inventory_item:
+            print("Nothing to delete.\n")
+            return inventory_item
+
+        delete = input("Enter item to delete: ").strip()
+        if delete not in inventory_item:
+            print(f"{delete} does not exist in the inventory.\n")
+            return inventory_item
+
+        inventory_item.pop(delete)
+        print(f"{delete} deleted!\n")
+        Inventory.write_to_file(inventory_item)
         return inventory_item
 
     def take_items() -> dict:
         """Take items from inventory."""
+        clear_screen()
         inventory_item = get_data()
-        unavaliable = "No inventory available!\n"
-        if inventory_item == {}:
-            print(unavaliable)
-        else:
-            try:
-                take = input("What did you take? ").strip()
-                take_quantity = int(input(f"How many {take}? "))
-                if take_quantity > inventory_item[take]:
-                    print(f"Not enough {take} available!\n")
-                elif take in inventory_item:
-                    inventory_item[take] -= take_quantity
-                    print(f"{inventory_item[take]} {take} left.\n")
-                    Inventory.write_to_file(inventory_item)
-                else:
-                    print(f"{take} doesn't exist.\n")
-            except ValueError:
-                print("Invalid entry!\n")
+        if not inventory_item:
+            print("No inventory available!\n")
+            return inventory_item
+
+        take = input("What did you take? ").strip()
+        if take not in inventory_item:
+            print(f"{take} does not exist in the inventory.\n")
+            return inventory_item
+
+        try:
+            take_quantity = int(input(f"How many {take}? "))
+        except ValueError:
+            print("Invalid entry!\n")
+            return inventory_item
+
+        if take_quantity > inventory_item[take]:
+            print(f"Not enough {take} available!\n")
+            return inventory_item
+
+        inventory_item[take] -= take_quantity
+        print(f"{inventory_item[take]} {take} left.\n")
+        Inventory.write_to_file(inventory_item)
         return inventory_item
 
     def view_items() -> None:
         """View items from inventory."""
+        clear_screen()
         view_items = get_data()
-        unavaliable = "No inventory available!\n"
-        if view_items == {}:
-            print(unavaliable)
-        else:
-            for item, view in view_items.items():
-                if int(view) < 0:
-                    print(unavaliable)
-                else:
-                    print(f'{item} = {view}')
-            print('\n')
+        if not view_items:
+            print("No inventory available!\n")
+            return
+
+        for item, view in view_items.items():
+            print(f'{item} = {view}')
+        print('\n')
