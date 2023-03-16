b3b095befd4386c7877898a2c987844b4ce84369
M. McConnaughey <mcmxl@reagan.com>
2023-03-15 20:19:45 -0600

    Update make_json_file.py

--- utils/make_json_file.py
+++ utils/make_json_file.py
@@ -1,19 +1,17 @@
-import os
+from os.path import exists
 
 class JsonFile:
     """Check and/or create inventory file."""
 
+    @staticmethod
     def create_inventory_file() -> None:
         """Create file"""
-        with open("inventory.json", "w+"):
-            if os.path.exists("inventory.json"):
-                print(f"inventory.json created!")
-            else:
-                return
+        with open("inventory.json", "w+") as f:
+            print(f"inventory.json created!")
 
+    @staticmethod
     def check_inventory_file() -> None:
         """Check if file exists"""
-        if os.path.exists("inventory.json"):
-            pass
-        else:
-            JsonFile.create_inventory_file()
+        if exists("inventory.json"):
+            return
+        JsonFile.create_inventory_file()
