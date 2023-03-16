8dd467e617a92f89860eb9c6f737a5e6ff58c9cb
M. McConnaughey <mcmxl@reagan.com>
2023-03-15 20:26:04 -0600

    Update menu.py

--- utils/menu.py
+++ utils/menu.py
@@ -1,14 +1,16 @@
 class Menu:
     """Prepare a menu."""
 
+    @staticmethod
     def add_numbers(num) -> None:
         """Add numbers to menu list."""
         for c, value in enumerate(num, 1):
             print(c, value)
 
+    @staticmethod
     def list_choices() -> list:
         """Give user a choice of actions."""
         inventory_actions = ["Add", "Take", "View", "Delete", "Exit"]
         Menu.add_numbers(inventory_actions)
         action_choice = input("What do you want to do? ")
-        return action_choice
+        return action_choice.strip().lower()
